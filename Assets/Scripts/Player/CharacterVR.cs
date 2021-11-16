a407650fae32e0c385eb0d429c072d8b17bd58c7
Kayakaga <baijidevelop@gmail.com>
2021-11-16 12:51:44 +0200

    VRmovement Stage1

---
+++ Assets/Scripts/Player/CharacterVR.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterVR : MonoBehaviour
+{
+    public float walkingSpeed = 7.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeedBase;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
+    [HideInInspector]
+    public bool canLook = true;
+
+    int jumps = 0;
+    Impact impactScript;
+
+    float mass = 3.0f; // defines the character mass
+    Vector3 impact = Vector3.zero;
+    CharacterController character;
+    float dashForce = 200f;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        lookSpeedBase = lookSpeed;
+        impactScript = GetComponent<Impact>();
+        character = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        DashUpdate();
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = canMove ? walkingSpeed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? walkingSpeed * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY) ;
+
+        if (Input.GetButtonDown("Jump") && canMove && (characterController.isGrounded || jumps < 1))
+        {
+            moveDirection.y = jumpSpeed;
+            jumps++;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+        else
+            if (jumps != 0) jumps = 0;
+
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canLook)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            AddImpact(transform.forward, dashForce);
+        }
+    }
+    void DashUpdate()
+    {
+        // apply the impact force:
+        if (impact.magnitude > 0.2) character.Move(impact * Time.deltaTime);
+        // consumes the impact energy each cycle:
+        impact = Vector3.Lerp(impact, Vector3.zero, 5 * Time.deltaTime);
+    }
+
+    public void AddImpact(Vector3 dir, float force)
+    {
+        dir.Normalize();
+        if (dir.y < 0) dir.y = -dir.y; // reflect down force on the ground
+        impact += dir.normalized * force / mass;
+    }
+}
